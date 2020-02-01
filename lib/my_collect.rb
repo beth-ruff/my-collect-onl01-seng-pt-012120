d61a04a40db89ed549b063a72c9482fed305eec0
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 19:54:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
+def my_collect(empty_array)
+  i = 0 
+  collection = []
+  while i < empty_array.length 
+    collection << yield(array[i])
+    i += 1 
+  end
+end
 
+def my_collect(languages)
+  i = 0 
+  collection = []
+  while i < languages.length 
+    collection << yield(languages[i])
+    i += 1 
+  end
+end
 
+my_collect(languases) {|i| i.upcase}
+
+def my_collect(students)
+  i = 0 
+  collection = []
+  while i < students.length 
+    collection << yield(students[i])
+    i += 1 
+  end
+end
+
+my_collect(students) {|i| i.split(" ").first}
